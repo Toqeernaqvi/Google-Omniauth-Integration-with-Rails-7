7c173a0b05f7edf63796bfb01cccca0c27799c57
toqeerabbas <toqeer@techsembly.com>
2023-07-22 15:21:10 +0500

    Google omniauth integration

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,7 @@
+# app/controllers/home_controller.rb
+class HomeController < ApplicationController
+  def index
+    # Add any necessary code here if needed
+    # The view will be rendered automatically
+  end
+end
